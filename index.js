506c56c067167a86c495409efa248c3091430a65
Taka005 <info@taka.ml>
2022-09-24 15:36:40 +0000

    編集:index.js 基本的なもの追加

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { Client, Intents } = require("discord.js");
+require("dotenv").config();
 
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
+});
+
+//ここに追加していく
+
+client.login(process.env.DISCORD_TOKEN)
+  .then(()=> console.info(`\x1b[34mINFO:ログインに成功しました`))
+  .catch(()=> console.error(`\x1b[31mERROR:ログインに失敗しました`))
+
+process.on('uncaughtException',async(error) =>{
+  console.error(`\x1b[31ERROR: ${error}`);
+  return;
+});
+
+process.on('unhandledRejection',async(error) =>{
+  console.error(`\x1b[31mERROR: ${error}`);
+  return;
+});
