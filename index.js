483f624f379b28950c1f559208d84b35aa58f332
Jampan1229 <67533379+Jampan1229@users.noreply.github.com>
2022-09-25 18:38:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ const client = new Client({
  * ./module/内に機能をファイル分け
  */
 
+fs.readdir(`./events/`, (err, files) => {
+	if (err) return logger.error(`An Error Occured while Loading Events. ${err.stack}`);
+	files.forEach((file) => {
+		if (!file.endsWith(`.js`)) return;
+		const event = require(`./events/${file}`);
+		let eventName = file.split(`.`)[0];
+		console.log(`[Event Manager]: Loading Event ${eventName}`);
+		client.on(eventName, event.bind(null, client));
+	});
+});
+
 client.login(process.env.DISCORD_TOKEN)
   .then(()=> console.info(`\x1b[34mINFO:ログインに成功しました`))
   .catch(()=> console.error(`\x1b[31mERROR:ログインに失敗しました`))
