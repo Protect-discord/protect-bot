7b3222301329c27fde112e5c9422d8a46675dd5c
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-09 06:36:02 +0900

    add: JSDoc

--- module/lib/config.js
+++ module/lib/config.js
@@ -1,5 +1,12 @@
 const fs = require('node:fs');
-
+/**
+ * @typedef {Object} configData
+ * @prop {Object} server
+ * @prop {Object} user
+ */
+/**
+ * @type {configData}
+ */
 const data = {
     server: JSON.parse(fs.readFileSync(`../../data/server.json`)),
     user: JSON.parse(fs.readFileSync(`../../data/user.json`))
@@ -13,4 +20,7 @@ const handler = {
         return JSON.parse(fs.readFileSync(`../../data/${prop}.json`));
     }
 }
+/**
+ * @type {configData}
+ */
 module.exports = new Proxy(data, handler)
