01eb05058645b12f90b53ac1f5a2c2fdd4b97cb4
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-09 06:31:29 +0900

    add: 設定に関するっライブラリ

---
+++ module/lib/config.js
@@ -0,0 +1,16 @@
+const fs = require('node:fs');
+
+const data = {
+    server: JSON.parse(fs.readFileSync(`../../data/server.json`)),
+    user: JSON.parse(fs.readFileSync(`../../data/user.json`))
+}
+const handler = {
+    get: (_obj, prop)=>{
+        return JSON.parse(fs.readFileSync(`../../data/${prop}.json`));
+    },
+    set: (obj, prop)=>{
+        fs.writeFileSync(`../../data/${prop}.json`, JSON.stringify(obj[prop], null,'  '));
+        return JSON.parse(fs.readFileSync(`../../data/${prop}.json`));
+    }
+}
+module.exports = new Proxy(data, handler)
