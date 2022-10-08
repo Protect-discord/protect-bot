7a0362de9bd8bd60e7d7a96295234f3974da1b00
Koro soft <104771756+Otatoge@users.noreply.github.com>
2022-10-09 06:58:20 +0900

    add: コマンド追加

--- module/ready/command.js
+++ module/ready/command.js
@@ -9,10 +9,10 @@ module.exports = async (client) => {
     SlashCommandBuilder,
     ContextMenuCommandBuilder,
   } = require("@discordjs/builders")
-  const {REST} = require("@discordjs/rest")
-  const {Routes, ApplicationCommandType} = require("discord-api-types/v10")
+  const { REST } = require("@discordjs/rest")
+  const { Routes, ApplicationCommandType } = require("discord-api-types/v10")
 
-  const rest = new REST({version: "10"}).setToken(process.env.DISCORD_TOKEN)
+  const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN)
 
   //スラッシュコマンド
   await rest.put(Routes.applicationCommands(client.application.id), {
@@ -20,10 +20,28 @@ module.exports = async (client) => {
       new SlashCommandBuilder()
         .setName("help")
         .setDescription("BOTの使い方を表示します"),
-      new SlashCommandBuilder().setName("report").setDescription("通報します"),
+      new SlashCommandBuilder()
+        .setName("report")
+        .setDescription("通報します"),
+      new ContextMenuCommandBuilder()
+        .setName('report'),
       new SlashCommandBuilder()
         .setName("scan")
         .setDescription("サーバーをスキャンし安全を確認します"),
+      new SlashCommandBuilder()
+        .setName("point")
+        .setDescription("評価値を設定します")
+        .addStringOption({
+          name: 'id',
+          description: '変更対象のid',
+          required: true
+        })
+        .addIntegerOption({
+          name: 'point',
+          description: '評価値', required: true,
+          max_value: 10,
+          min_value: 0
+        }),
     ],
   })
 }
