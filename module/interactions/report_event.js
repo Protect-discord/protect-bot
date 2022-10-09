26b17f0b781150f9909d146176d364d1681f1a03
Taka005 <info@taka.ml>
2022-10-09 14:23:19 +0000

    Revert "モーダルとの互換性を維持"
    This reverts commit 6595b9e839a8e6aca638405e58493b1f04ddf20d.

---
+++ module/interactions/report_event.js
@@ -0,0 +1,43 @@
+module.exports = async (interaction, client) => {
+  const {report_channel} = require("../../config.json")
+  if (!interaction.isModalSubmit()) return
+  if (interaction.customId === "report") {
+    const content = await interaction.fields.getTextInputValue("content")
+
+    await client.channels.cache.get(report_channel).send({
+      embeds: [
+        {
+          color: "GREEN",
+          author: {
+            name: `${interaction.member.user.id}`,
+            url: `https://discord.com/users/${interaction.member.user.id}`,
+            icon_url:
+              interaction.member.user.avatarURL() ||
+              "https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: content,
+          footer: {
+            text: `${interaction.guild.name}<${interaction.guild.id}>`,
+            icon_url:
+              interaction.guild.iconURL() ||
+              "https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          timestamp: new Date(),
+        },
+      ],
+    })
+    await interaction.reply({
+      embeds: [
+        {
+          author: {
+            name: "送信しました",
+            icon_url: "https://cdn.taka.ml/images/success.png",
+          },
+          color: "GREEN",
+          description: "ご報告ありがとうございました",
+        },
+      ],
+      ephemeral: true,
+    })
+  }
+}
